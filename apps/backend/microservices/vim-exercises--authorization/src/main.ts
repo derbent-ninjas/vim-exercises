25d01966ceddeef9abe394bb1ebefd3c17996916
TbotaPhantA <botakiller@mail.ru>
2022-01-26 00:42:23 +0300

    authorization microservice was created. It needs configuration.

--- apps/backend/microservices/vim-exercises--authorization/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { VimExercisesAuthorizationModule } from './vim-exercises--authorization.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(VimExercisesAuthorizationModule);
-  await app.listen(3000);
-}
-bootstrap();
