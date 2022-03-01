b21d0026ec6d8fe7dd1d39de1a137b3d7b151ffe
TbotaPhantA <botakiller@mail.ru>
2022-03-01 11:50:35 +0300

    config type was added

--- apps/backend/ve--boilerplate/src/config.ts
+++ apps/backend/ve--boilerplate/src/config.ts
@@ -1,9 +1,10 @@
+import { AppConfig } from './shared/types/appConfig'
 
 const isRequired = (propName: string): never => {
   throw new Error(`Config property ${propName} is required`)
 }
 
-export default () => ({
+export default (): AppConfig => ({
   server: {
     host: process.env.HOST ?? isRequired('HOST'),
     port: Number(process.env.REST_PORT) ?? isRequired('REST_PORT'),
