b7caab072bbd0f55994a023db981a469137ef307
TbotaPhantA <botakiller@mail.ru>
2022-05-09 17:23:53 +0000

    text property was added to editor

--- packages/vim-emulator/src/editor/editor.ts
+++ packages/vim-emulator/src/editor/editor.ts
@@ -3,5 +3,6 @@ import { Cursor } from './cursor'
 export class Editor {
   constructor(
     private readonly cursor: Cursor,
+    private readonly text: string,
   ) {}
 }
--- packages/vim-emulator/tests/editor/editor.spec.ts
+++ packages/vim-emulator/tests/editor/editor.spec.ts
@@ -4,11 +4,11 @@ import { Coordinate } from '../../src/editor/coordinate'
 
 describe('Editor', () => {
   describe('constructor', () => {
-    test('When passed cursor - should be created successfully', () => {
+    test('When passed cursor and empty text - should be created successfully', () => {
       const cursorCoordinate = new Coordinate(1, 1)
       const cursor = new Cursor(cursorCoordinate)
 
-      expect(() => new Editor(cursor)).not.toThrow()
+      expect(() => new Editor(cursor, '')).not.toThrow()
     })
   })
 })
